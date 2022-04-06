fedc8da7f5e1b4e68c3995d331fd068034d16ddf
Cepheus-king <84628709+cepheus-king@users.noreply.github.com>
2022-04-06 10:29:20 -0700

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
-export default function getResponseFromAPI(){
- return new Promise((resolve) => {
-   resolve('msg');
+export default function getResponseFromAPI() {
+ return new Promise((resol) => {
+   resol('msg');
  });
 }
